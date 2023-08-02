882f042a5f7fde2441792c7984d455fe551acce8
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2023-08-02 16:02:57 -0300

    Creamos una forma de hacer SPAs (Single Page Application)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import { useState } from 'react'
 import './App.css'
+import { useEffect } from 'react'
+
+const NAVIGATION_EVENT = 'pushstate'
+
+function navigate (href) {
+// history para cambiar la url sin recargar la pagina, solo reflejar el cambio en la url
+    window.history.pushState({}, '', href)
+// creamos un evento personalizado
+    const navigationEvent = new Event(NAVIGATION_EVENT) 
+    window.dispatchEvent(navigationEvent) // enviamos el evento
+}
 
 function HomePage(){
     return (
         <>
             <h1>Home</h1>
             <p>Esta es una pagina de ejemplo para crear un React Router</p>
-            <a href="/about">IR a sobre nosotros</a>
+            <a onClick={() => navigate('/about')}>IR a sobre nosotros</a>
         </>
     )
 }
@@ -19,7 +30,7 @@ function AboutPage(){
                 <img src="https://yt3.ggpht.com/yti/AOXPAcWxyho-M7fI7bjPbdteU9ULNSmEoedWy9JgvEql_A=s88-c-k-c0x00ffffff-no-rj" alt="foto"></img>
                 <p>Hola Soy Carlos y estoy aprendiendo a usar React Router</p>
             </div>
-            <a href="/">Ir al home</a>
+            <a onClick={() => navigate('/')}>Ir al home</a>
         </>
     )
 }
@@ -27,6 +38,20 @@ function AboutPage(){
 
 function App() {
     const [currentPath, setCurrentPath] = useState(window.location.pathname)
+
+    // suscripcion al evento pushstate
+    useEffect(() => {
+// esto se ejecuta cada vez que hay un cambio en la navegacion y lo hace al escuchar el evento
+        const onLocationChange = () => {
+            setCurrentPath(window.location.pathname)
+        }
+        window.addEventListener(NAVIGATION_EVENT, onLocationChange)
+        // despues limpiamos el evento
+        return () => {
+            window.removeEventListener(NAVIGATION_EVENT, onLocationChange)
+        }
+    }, [])
+
     return (
         <main>
             {currentPath === '/' && <HomePage />}
