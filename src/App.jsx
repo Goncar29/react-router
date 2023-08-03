a989eb6a4edf4c6d6951f6fc97b4e325b8c6388d
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2023-08-03 17:26:42 -0300

    Creamos componente Router para hacerlo mas declarativo y soportar ruta por defecto (404)

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,24 @@
-import { useState, useEffect } from 'react'
 import './App.css'
-import { EVENTS } from './const'
 import HomePage from './pages/Home'
 import AboutPage from './pages/About'
+import Page404 from './pages/404'
+import { Router } from './Router'
+
+const appRoutes = [
+    { 
+        path: '/',
+        component: HomePage
+    },
+    { 
+        path: '/about',
+        component: AboutPage
+    }
+]
 
 function App() {
-    const [currentPath, setCurrentPath] = useState(window.location.pathname)
-
-    // suscripcion al evento pushstate
-    useEffect(() => {
-// esto se ejecuta cada vez que hay un cambio en la navegacion y lo hace al escuchar el evento
-        const onLocationChange = () => {
-            setCurrentPath(window.location.pathname)
-        }
-
-        window.addEventListener(EVENTS.PUSHSTATE, onLocationChange)
-        window.addEventListener(EVENTS.POPSTATE, onLocationChange) // evento cuando vamos para atras
-        
-        return () => {  // despues limpiamos el evento
-            window.removeEventListener(EVENTS.PUSHSTATE, onLocationChange)
-            window.removeEventListener(EVENTS.POPSTATE, onLocationChange)
-        }
-    }, [])
-
     return (
         <main>
-            {currentPath === '/' && <HomePage />}
-            {currentPath === '/about' && <AboutPage />}
+            <Router routes={appRoutes} defaultComponent={Page404} />
         </main>
     )
 }
